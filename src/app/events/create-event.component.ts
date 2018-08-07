99da776dc9e453c844da7a903f693ad21cb7b849
Aditya <aditya.magotra@gmail.com>
2018-08-08 00:28:12 +0530

    Added images and updated packages.

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
-import { fromEvent } from '../../../node_modules/rxjs/observable/fromEvent';
 import { EventService } from './shared';
 
 @Component({
