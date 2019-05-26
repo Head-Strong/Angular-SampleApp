8deec09f1583dcfe7d60ddcd8428f61431ed939e
Aditya Magotra <aditya.magotra@gmail.com>
2019-05-26 11:40:14 +0100

    Create accessdenied.component.ts

---
+++ src/app/common/accessdenied.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl:'./accessdenied.component.html'
+})
+export class AccessDeniedComponent{
+  constructor() {
+    var data = 1;
+  }
+}
