b5bb9f67b6ed2ae7c103a6345827a6a7efd11335
Aditya Magotra <aditya.magotra@gmail.com>
2019-05-26 11:43:08 +0100

    Create DateTimeFormatPipe.ts

---
+++ src/app/common/DateTimeFormatPipe.ts
@@ -0,0 +1,17 @@
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Constants } from './constants';
+
+@Pipe({
+  name: 'dateTimeFormat'
+})
+export class DateTimeFormatPipe extends DatePipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    try{
+    return super.transform(value, Constants.DATE_TIME_FMT);
+    }
+    catch(error){
+      return value;
+    }
+  }
+}
